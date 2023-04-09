26dbf91e208139da12fa073bb4d090af11006de1
razor <sgavrilov387@gmail.com>
2023-04-09 10:13:11 -0400

    spinner AddFragment.kt

--- app/src/main/java/com/example/dictionary/presentation/fragment/AddFragment.kt
+++ app/src/main/java/com/example/dictionary/presentation/fragment/AddFragment.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
 import androidx.fragment.app.Fragment
 import com.example.dictionary.R
 import com.example.dictionary.databinding.FragmentAddBinding
@@ -32,13 +33,16 @@ class AddFragment : Fragment() {
                     addWord(
                         Word(
                             english = binding.editTextEnglish.text.toString().trim(),
-                            russian = binding.editTextRussian.text.toString().trim()
+                            russian = binding.editTextRussian.text.toString()
+                                .trim() + binding.spinnerRussian.selectedItem.toString().trim()
                         )
                     )
                     (activity as MainActivity).navigateAction(R.id.menu1)
                 }
             }
         }
+
+        spinner()
 
         return binding.root
     }
@@ -50,6 +54,17 @@ class AddFragment : Fragment() {
 
     private fun addWord(word: Word) {
         viewModel.addWord(word = word)
+    }
+
+    private fun spinner() {
+        ArrayAdapter.createFromResource(
+            requireContext(),
+            R.array.parts_of_speech_array,
+            android.R.layout.simple_spinner_item
+        ).also { adapter ->
+            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+            binding.spinnerRussian.adapter = adapter
+        }
     }
 
 }
